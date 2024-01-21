7cacf7881038bea7bf32d36a0a543582a5dfe11c
whitepaper907 <101ghgh@naver.com>
2024-01-21 19:28:16 +0900

    feat : get hello

--- src/main/java/bjm/ci/cd/project/controller/MemberController.java
+++ src/main/java/bjm/ci/cd/project/controller/MemberController.java
@@ -2,16 +2,17 @@ package bjm.ci.cd.project.controller;
 
 import bjm.ci.cd.project.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    @GetMapping("")
+    public String hello(){
+        return "hello";
+    }
 
     @PostMapping("/add")
     public void addMember(@RequestParam(value = "nickname") String nickname){
